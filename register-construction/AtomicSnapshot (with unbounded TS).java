732a2ece0973adcf668ba5a91209e3cfbda0fb76
Huy-DNA <huydo862003@gmail.com>
2025-06-23 16:03:02 +0700

    feat: add atomic snapshot

---
+++ register-construction/AtomicSnapshot (with unbounded TS).java
@@ -0,0 +1,63 @@
+class StampedSnapshot {
+  long stamp; // unbounded timestamp
+  byte value;
+  byte[] snap;
+
+  StampedSnapshot(long stamp, byte value, byte[] snap) {
+    this.stamp = stamp;
+    this.value = value;
+    this.snap = snap;
+  }
+}
+
+public class AtomicSnapshot {
+  StampedSnapshot registers[]; // MRMW atomic registers
+
+  public AtomicSnapshot(int nregisters) {
+    this.registers = new StampedSnapshot[nregisters];
+    for (int i = 0; i < nregisters; ++i) {
+      byte[] snap = new byte[nregisters];
+      this.registers[i] = new StampedSnapshot(0, (byte) 0, snap);
+    }
+  }
+
+  private StampedSnapshot[] collect() {
+    StampedSnapshot[] copy = new StampedSnapshot[registers.length];
+    for (int i = 0; i < this.registers.length; ++i) {
+      copy[i] = registers[i];
+    }
+    return copy;
+  }
+
+  public void update(byte newValue, int index) {
+    byte[] snap = scan();
+    StampedSnapshot oldValue = registers[index];
+    registers[index] = new StampedSnapshot(oldValue.stamp + 1, newValue, snap);
+  }
+
+  public byte[] scan() {
+    StampedSnapshot[] oldSnaps = this.collect();
+    StampedSnapshot[] newSnaps;
+    boolean moved[] = new boolean[this.registers.length];
+    collect: while (true) {
+      newSnaps = this.collect();
+      for (int i = 0; i < oldSnaps.length; ++i) {
+        if (oldSnaps[i].stamp != newSnaps[i].stamp || oldSnaps[i].value != newSnaps[i].value) {
+          if (moved[i]) {
+            return oldSnaps[i].snap;
+          } else {
+            moved[i] = true;
+            oldSnaps = newSnaps;
+            continue collect;
+          }
+        }
+      }
+
+      byte result[] = new byte[this.registers.length];
+      for (int i = 0; i < this.registers.length; ++i) {
+        result[i] = newSnaps[i].value;
+      }
+      return result;
+    }
+  }
+}
